b61bf371639fbc6e3153ae35b2bbf2530b94545f
unknown <trabelsi.mouhib3@gmail.com>
2021-06-14 14:45:38 +0100

    fixing errors

--- Back-end/app.js
+++ Back-end/app.js
@@ -36,10 +36,10 @@ app.use('/api/mall', payment)
 app.use('/api/mall', newsletter)
 
 if (process.env.NODE_ENV === 'PRODUCTION') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.use(express.static(path.join(__dirname, '../Front_end/build')))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+        res.sendFile(path.resolve(__dirname, '../Front_end/build/index.html'))
     })
 }
 
