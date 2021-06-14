6792ab331cadc28b9c8be73054c7cc1b43f1c617
unknown <trabelsi.mouhib3@gmail.com>
2021-06-14 20:33:08 +0100

    fixing bugs

--- Back-end/app.js
+++ Back-end/app.js
@@ -36,13 +36,7 @@ app.use('/api/mall', events)
 app.use('/api/mall', payment)
 app.use('/api/mall', newsletter)
 
-if (process.env.NODE_ENV === 'PRODUCTION') {
-    app.use(express.static(path.join(__dirname, '../Front_end/build')))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../Front_end/build/index.html'))
-    })
-}
 
 
 // Middleware to handle errors
